650b3367816e4b42584f65ccd98b46972bed80c7
gengch <gengchuanhua@163.com>
2023-10-24 16:45:24 +0800

    [update] 添加测试代码

---
+++ Runtime/GFramework.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GFramework
+{
+    public class GFramework
+    {
+        public static void TestPrint()
+        {
+            Debug.LogError("==> [TestPrint]");
+        }
+    }
+}
---
+++ Samples/GFramework Sample/GFrameworkTest.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GFrameworkTest : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        GFramework.GFramework.TestPrint();
+    }
+}
