4c5037e8e66cca2d9680fa53d37475818d4e7a23
gengch <gengchuanhua@163.com>
2023-10-25 13:46:02 +0800

    [update] 更新GFramework.asmdef

--- Assets/GFramework/Runtime/AssemblyInfo.cs
+++ Assets/GFramework/Runtime/AssemblyInfo.cs
@@ -1,6 +1,3 @@
 ﻿using System.Runtime.CompilerServices;
 
-[assembly: InternalsVisibleTo("YooAsset.Editor")]
-[assembly: InternalsVisibleTo("YooAsset.EditorExtension")]
-[assembly: InternalsVisibleTo("YooAsset.RuntimeExtension")]
-[assembly: InternalsVisibleTo("Assembly-CSharp-Editor")]
+[assembly: InternalsVisibleTo("UnityEngine.Purchasing.Security")]
