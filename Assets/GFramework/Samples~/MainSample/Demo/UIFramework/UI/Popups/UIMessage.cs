dac9c534c5ffd7ce31c10680527bf528a029dbf0
gengch <gengchuanhua@163.com>
2024-06-07 14:58:42 +0800

    [update] 更新 Samples

--- Assets/GFramework/Samples~/MainSample/Demo/UIFramework/UI/Popups/UIMessage.cs
+++ Assets/GFramework/Samples~/MainSample/Demo/UIFramework/UI/Popups/UIMessage.cs
@@ -1,12 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Framework;
 
 namespace GameFramework
 {
     public class UIMessage : UIBaseMessage
     {
-        
+        protected IGameModel gameModel;
+        protected ISettingModel settingModel;
+        protected IStatisticsModel statisticsModel;
+        protected override void MonoAwake()
+        {
+            base.MonoAwake();
+            gameModel = this.GetModel<IGameModel>();
+            settingModel = this.GetModel<ISettingModel>();
+            statisticsModel = this.GetModel<IStatisticsModel>();
+        }
     }
 }
 
