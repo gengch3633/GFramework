5a430c0ad267ceb4db72cbabf644ee65179d9abe
gengch <gengchuanhua@163.com>
2024-09-09 21:50:40 +0800

    [update] AdLoadingPopup、PurchaseLoadingPopup

---
+++ Assets/GFramework/Runtime/GameFramework/UIFramework/UI/Popups/AdLoadingPopup.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using GameFramework;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Solitaire
+{
+    public class AdLoadingPopup : UIBasePopup
+    {
+
+    }
+}
+
---
+++ Assets/GFramework/Runtime/GameFramework/UIFramework/UI/Popups/PurchaseLoadingPopup.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using GameFramework;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Solitaire
+{
+    public class PurchaseLoadingPopup : UIBasePopup
+    {
+        private float rotateAngle = 240;
+        private float currentAngle = 0;
+        private void Update()
+        {
+            currentAngle += rotateAngle * Time.deltaTime;
+            iconLoadingVar.transform.localEulerAngles = new Vector3(0, 0, currentAngle);
+        }
+        private Image iconLoadingVar;
+        protected override void OnAddUIListeners()
+        {
+            base.OnAddUIListeners();
+            iconLoadingVar = transform.Find("bg/IconLoading_Var").GetComponent<Image>();
+
+        }
+        protected override void OnRemoveUIListeners()
+        {
+            base.OnRemoveUIListeners();
+
+        }
+    }
+}
+
