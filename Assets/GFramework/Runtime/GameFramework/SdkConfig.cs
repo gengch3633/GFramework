8d4dda3dcf8ad452737795dd53ba87be95bf6361
gengch <gengchuanhua@163.com>
2024-09-28 16:44:48 +0800

    [update] SdkConst

--- Assets/GFramework/Runtime/GameFramework/SdkConfig.cs
+++ Assets/GFramework/Runtime/GameFramework/SdkConfig.cs
@@ -6,8 +6,10 @@ namespace GameFramework
         public string supportEmail;
         public string privacyPage;
 
-        public string appsFlyerKey;
         public string facebookAppId;
         public string facebookToken;
+
+        public string tenjinSdkKey;
+        public string appsFlyerKey;
     }
 }
