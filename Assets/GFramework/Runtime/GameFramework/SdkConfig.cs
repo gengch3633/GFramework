32124e97989fd4005483af88b9c322185a809807
gengch <gengchuanhua@163.com>
2024-09-28 16:14:42 +0800

    [update] SdkConst

--- Assets/GFramework/Runtime/GameFramework/SdkConfig.cs
+++ Assets/GFramework/Runtime/GameFramework/SdkConfig.cs
@@ -3,17 +3,11 @@ namespace GameFramework
     public class SdkConfig
     {
         public int id;
-        public bool isTest;
         public string supportEmail;
         public string privacyPage;
-        public string appsFlyerKey;
 
+        public string appsFlyerKey;
         public string facebookAppId;
         public string facebookToken;
-
-        public string admobAppId;
-        public string admobRewardAdId;
-        public string admobInterAdId;
-        public string admobBannerAdId;
     }
 }
--- Assets/GFramework/Runtime/GameFramework/UIFramework/Systems/SDKConst.cs
+++ Assets/GFramework/Runtime/GameFramework/UIFramework/Systems/SDKConst.cs
@@ -7,18 +7,11 @@ namespace GameFramework
     public class SDKConst
     {
         private static SdkConfig sdkConfigProduction;
-        private static SdkConfig sdkConfigDevelopment;
 
         public static SdkConfig SdkConfigProduction { get => sdkConfigProduction; }
-        public static SdkConfig SdkConfigDevelopment { get => sdkConfigDevelopment; }
         static SDKConst()
         {
-            var sdkConfigs = GameUtils.GetConfigInfos<SdkConfig>();
-            sdkConfigProduction = sdkConfigs.Find(item=> !item.isTest);
-            sdkConfigDevelopment = sdkConfigs.Find(item => item.isTest);
+            sdkConfigProduction = GameUtils.GetConfigInfo<SdkConfig>();
         }
-
-        public const string SDK_APPSFLYER_LINKID = "";
-        public const string SDK_APPSFLYER_IOS_APPID = "";
     }
 }
