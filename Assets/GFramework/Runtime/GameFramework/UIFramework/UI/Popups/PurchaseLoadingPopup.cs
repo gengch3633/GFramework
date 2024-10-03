56313cb22712b98fb709bb7a6fd391863f7312c5
gengch <gengchuanhua@163.com>
2024-10-03 21:27:33 +0800

    [update] PurchaseLoadingPopup

--- Assets/GFramework/Runtime/GameFramework/UIFramework/UI/Popups/PurchaseLoadingPopup.cs
+++ Assets/GFramework/Runtime/GameFramework/UIFramework/UI/Popups/PurchaseLoadingPopup.cs
@@ -8,25 +8,7 @@ namespace GameFramework
 {
     public class PurchaseLoadingPopup : UIBasePopup
     {
-        private float rotateAngle = 240;
-        private float currentAngle = 0;
-        private void Update()
-        {
-            currentAngle += rotateAngle * Time.deltaTime;
-            iconLoadingVar.transform.localEulerAngles = new Vector3(0, 0, currentAngle);
-        }
-        private Image iconLoadingVar;
-        protected override void OnAddUIListeners()
-        {
-            base.OnAddUIListeners();
-            iconLoadingVar = transform.Find("bg/IconLoading_Var").GetComponent<Image>();
-
-        }
-        protected override void OnRemoveUIListeners()
-        {
-            base.OnRemoveUIListeners();
-
-        }
+       
     }
 }
 
