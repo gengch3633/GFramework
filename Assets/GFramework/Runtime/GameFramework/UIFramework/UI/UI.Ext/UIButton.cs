588ffd836fad4e63c125f3a4de8b0d1bcd3296fb
gengchuanhua@163.com <gengch>
2024-12-30 09:57:06 +0800

    [update] UIButton

--- Assets/GFramework/Runtime/GameFramework/UIFramework/UI/UI.Ext/UIButton.cs
+++ Assets/GFramework/Runtime/GameFramework/UIFramework/UI/UI.Ext/UIButton.cs
@@ -9,12 +9,11 @@ namespace GameFramework
 {
     public class UIButton : Button
     {
-        public List<GameObject> interactableObjects = new List<GameObject>();
-        public List<GameObject> notInteractableObjects = new List<GameObject>();
-        protected override async void Awake()
+        private List<GameObject> interactableObjects = new List<GameObject>();
+        private List<GameObject> notInteractableObjects = new List<GameObject>();
+        protected override void Awake()
         {
             base.Awake();
-            await UniTask.DelayFrame(1);
             var childTransforms = GetComponentsInChildren<Transform>().ToList();
             var intTransforms = childTransforms.FindAll(item => item.name.Contains("_Int"));
             var notIntTransforms = childTransforms.FindAll(item => item.name.Contains("_NotInt"));
@@ -24,6 +23,24 @@ namespace GameFramework
         protected override void DoStateTransition(SelectionState state, bool instant)
         {
             base.DoStateTransition(state, instant);
+            SetBtnsState(state, instant).Forget();
+        }
+        
+        private async UniTask SetBtnsState(SelectionState state, bool instant)
+        {
+            interactableObjects.ForEach(item => item.SetActive(state != SelectionState.Disabled));
+            notInteractableObjects.ForEach(item => item.SetActive(state == SelectionState.Disabled));
+
+            var loopCount = 0;
+
+            while (interactableObjects.Count == 0 || notInteractableObjects.Count == 0)
+            {
+                await UniTask.DelayFrame(1);
+                loopCount++;
+                if (loopCount > 5)
+                    break;
+            }
+
             interactableObjects.ForEach(item => item.SetActive(state != SelectionState.Disabled));
             notInteractableObjects.ForEach(item => item.SetActive(state == SelectionState.Disabled));
         }
